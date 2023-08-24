f7d5bf8e8e4e7db292748ba405ec1c077b18e57e
SamaraneGabriel <samarane.gabriel@gmail.com>
2023-08-24 16:31:34 -0300

    Create Soma.java

---
+++ src/main/java/exercicio1/Soma.java
@@ -0,0 +1,25 @@
+package exercicio1;
+
+import java.util.*;
+
+public class Soma {
+	
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String args[]) {
+		
+		int a = 0;
+		int b = 0;
+		System.out.println("Digite um numero: ");
+		a = sc.nextInt();
+		System.out.println("Digite outro numero: ");
+		b = sc.nextInt();
+				
+		
+		
+		
+		System.out.println("A soma dos dois numeros é: "+(a+b));
+	
+	}
+	
+}
